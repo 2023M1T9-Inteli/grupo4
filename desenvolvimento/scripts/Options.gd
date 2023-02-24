d2fa4dca4c4b920557ce62f509834df9403e7c2b
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-02-24 20:44:53 -0300

    Adição de boas práticas de espaçamento de funções

--- desenvolvimento/scripts/Options.gd
+++ desenvolvimento/scripts/Options.gd
@@ -9,6 +9,7 @@ func _ready():
 	# EN_US: Sync slider to music volume when opening
 	$VolumeGrid/SliderMusic.value = AudioServer.get_bus_volume_db(master_bus)
 
+
 # PT_BR: Controla o volume da música e a muta 
 # EN_US: Controls music volume and mute
 func _on_SliderMusic_value_changed(value):
@@ -18,6 +19,7 @@ func _on_SliderMusic_value_changed(value):
 		AudioServer.set_bus_mute(master_bus,true)
 	else:
 		AudioServer.set_bus_mute(master_bus,false)
+
 
 # PT_BR: Abre a cena do menu
 # EN_US: Open the menu scene
