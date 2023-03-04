7933ff167959704c88c58c09fbcb481530d3e3ad
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-03-04 16:19:26 -0300

    Adição do efeito 'hover' na tela de início

--- desenvolvimento/scripts/Menu.gd
+++ desenvolvimento/scripts/Menu.gd
@@ -3,17 +3,17 @@ extends Control
 
 # PT_BR: Inicia o jogo
 # EN_US: Start the game
-func _on_StartButton_pressed():
+func _on_HoverStartButton_pressed():
 	get_tree().change_scene("res://scenes/Phases.tscn")
 
 
 # PT_BR: Abre a cena de opções
 # EN_US: Open the options scene
-func _on_OptionsButton_button_up():
+func _on_HoverOptionsButton_pressed():
 	get_tree().change_scene("res://scenes/Options.tscn")
 
 
 # PT_BR: Abre a cena de créditos
 # EN_US: Opens the credits scene
-func _on_CreditsButton_button_up():
+func _on_HoverCreditsButton_pressed():
 	get_tree().change_scene("res://scenes/Credits.tscn")
--- desenvolvimento/scripts/buttons/ExitButton.gd
+++ desenvolvimento/scripts/buttons/ExitButton.gd
@@ -2,5 +2,5 @@ extends Button
 
 # PT_BR: Ao clicar o jogo fechará
 # EN_US: By clicking the game will close
-func _on_ExitButton_pressed():
+func _on_HoverExitButton_pressed():
 	get_tree().quit()
