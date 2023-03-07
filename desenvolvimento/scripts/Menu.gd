63a67734d315fab9461083ea70ad211e5702280c
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-03-07 15:25:42 -0300

    Alteração nos nós da tela de Início

--- desenvolvimento/scripts/Menu.gd
+++ desenvolvimento/scripts/Menu.gd
@@ -3,17 +3,17 @@ extends Control
 
 # PT_BR: Inicia o jogo
 # EN_US: Start the game
-func _on_HoverStartButton_pressed():
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://scenes/Phases.tscn")
 
 
 # PT_BR: Abre a cena de opções
 # EN_US: Open the options scene
-func _on_HoverOptionsButton_pressed():
+func _on_OptionsButton_pressed():
 	get_tree().change_scene("res://scenes/Options.tscn")
 
 
 # PT_BR: Abre a cena de créditos
 # EN_US: Opens the credits scene
-func _on_HoverCreditsButton_pressed():
+func _on_CreditsButton_pressed():
 	get_tree().change_scene("res://scenes/Credits.tscn")
