663e539a89f255e50e79ca3bd07e99bbc45a075f
uMorita <pedromorita19@gmail.com>
2023-03-08 02:08:37 -0300

    Alterações do Process

--- desenvolvimento/scripts/base_phase/PCProgressing.gd
+++ desenvolvimento/scripts/base_phase/PCProgressing.gd
@@ -9,4 +9,4 @@ func _ready():
 func pc_processing():
 	self.value += 9
 	if self.value == 81:
-		get_tree().change_scene("res://scenes/Victory.tscn")
+		get_tree().change_scene("res://scenes/base_phase/Victory.tscn")
