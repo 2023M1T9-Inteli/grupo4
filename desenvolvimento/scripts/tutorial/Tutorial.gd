8533fc7f711cd320850d91937feacd603b163f5e
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-03-08 08:32:25 -0300

    Conserto de dependências da cena de tutorial

--- desenvolvimento/scripts/tutorial/Tutorial.gd
+++ desenvolvimento/scripts/tutorial/Tutorial.gd
@@ -5,4 +5,4 @@ func _on_next_button_pressed():
 	get_tree().change_scene("res://scenes/tutorial/introduction_to_file.tscn")
 
 func _on_Skip_button_pressed():
-	get_tree().change_scene("res://Cenas/Progress.tscn")
+	get_tree().change_scene("res://scenes/phase1/Phase1.tscn")
--- desenvolvimento/scripts/tutorial/introduction_to_files.gd
+++ desenvolvimento/scripts/tutorial/introduction_to_files.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_back_button_pressed():
-	get_tree().change_scene("res://Cenas/introduction_to_file.tscn")
+	get_tree().change_scene("res://scenes/tutorial/Tutorial.tscn")
 
 func _on_next_button_pressed():
-	get_tree().change_scene("res://Cenas/Gif_drag_and_drop.tscn")
+	get_tree().change_scene("res://scenes/tutorial/introduction_to_task.tscn")
