9a61da7c09e479365b7b938a995ecf958d29973f
raissavieiram <raissa.melo@sou.inteli.edu.br>
2023-03-10 11:13:51 -0300

    Comentei os códigos as pastas: drawer, phase 1 e tutorial.

--- desenvolvimento/scripts/tutorial/TaskTutorial.gd
+++ desenvolvimento/scripts/tutorial/TaskTutorial.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+#PT_BR: Função que troca a cena do tutorial para a cena anterior.
+#EN_US: Function that changes the tutorial scene with the previus tutorial scene.
 func _on_back_button2_pressed():
 	get_tree().change_scene("res://scenes/tutorial/FileTutorial.tscn")
 
 
+#PT_BR: Função que muda a cena do tutorial, de apresentação, para a próxima cena do tutorial.
+#EN_US: Function that changes the apresentation tutorial scene to the next scene of tutorial.
 func _on_next_button2_pressed():
 	get_tree().change_scene("res://scenes/tutorial/DragAndDropTutorial.tscn")
