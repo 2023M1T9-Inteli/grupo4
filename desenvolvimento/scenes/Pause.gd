11c35fecfb4c0d4f3ccfe22b3b02caef9fd99b40
Pedro Morita Bannwart <pedro.bannwart@sou.inteli.edu.br>
2023-03-15 15:15:35 -0300

    adição de novos arquivos para a tela de pause

---
+++ desenvolvimento/scenes/Pause.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+var is_paused = false setget set_is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+
+func _on_ContinueButton_pressed():
+	self.is_paused = false
+	self.hide()
+	
+
+func _on_OptionsButton_pressed():
+	$Options.show()
+	
+
+func _on_QuitButton_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn")
+
+
+func _on_ExitButton_pressed():
+	self.is_paused = !is_paused	
+	pass # Replace with function body.
