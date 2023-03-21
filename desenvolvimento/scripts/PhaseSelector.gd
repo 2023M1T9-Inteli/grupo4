7211d40eaf68e16157f8f386bd685a65a79a0f7c
raissavieiram <raissa.melo@sou.inteli.edu.br>
2023-03-21 17:33:30 -0300

    Fiz o desbloqueio da segunda fase e alterei os nomes dos arquivos da pasta de assets

---
+++ desenvolvimento/scripts/PhaseSelector.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _ready():
+	if Globals.player_score_phase_1 >= 850:
+		#mudar a texture do botão
+		pass
+
+# PT_BR: Abre a cena do level 1
+# EN_US: Opens the level 1 scene
+func _on_Phase1Button_pressed():
+	get_tree().change_scene("res://scenes/Dialog/DialogScene1.tscn")
+
+
+func _on_Phase2Button_pressed():
+	if Globals.player_score_phase_1 >= 850:
+		get_tree().change_scene("res://scenes/Dialog/DialogScene1.tscn")
