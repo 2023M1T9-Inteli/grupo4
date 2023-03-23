6fa4931253f90c1468a3fcd78bb486c077357c0b
Pedro Morita Bannwart <pedro.bannwart@sou.inteli.edu.br>
2023-03-23 16:11:49 -0300

    Feedback arrumado e tarefas organizadas

--- desenvolvimento/scripts/Result/Defeat.gd
+++ desenvolvimento/scripts/Result/Defeat.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 func _ready():
+#PT/BR: Define o valor da "$ScoreBar" como o score da fase um, além de calcular "result" e mostrar na tela
+#EN/US: Defines the value of "$ScoreBar" as the score of phase one, also calculation the "result" and showing it on screen
 	$ScoreBar.value = Globals.score_phase_1
 	var result := float(Globals.score_phase_1 * 100) / float(Globals.max_score_phase_1)
 	$FinalScore.text = ("%00.0f" % result) + "%"
 
 
-# PT_BR: Abre a cena de Fases
-# EN_US: Open the phases scene
+# PT_BR: Abre a cena de Fases e reinicia a variável "Global.score_phase_1"
+# EN_US: Open the phases scene and resets the variable "Global.score_phase_1"
 func _on_BackButton_pressed():
 	Globals.score_phase_1 = 0
 	get_tree().change_scene("res://scenes/Phases.tscn")
