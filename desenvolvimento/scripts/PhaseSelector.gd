763cff786a4eaefa5042ad2bfd0bdb9c85c40bca
Yan M. Coutinho <yan.m.coutinho@gmail.com>
2023-03-23 09:13:44 -0300

    Alteração de alguns scripts para arrumar bugs feitos durante o merge

--- desenvolvimento/scripts/PhaseSelector.gd
+++ desenvolvimento/scripts/PhaseSelector.gd
@@ -5,11 +5,11 @@ onready var phase_1_musica = preload ("res://assets/Audio/music_phase_1.mp3")
 var result = 0
 
 func _ready():
-  result = float(Globals.score_phase_1 * 100) / float(Globals.max_score_phase_1)
+	result = float(Globals.score_phase_1 * 100) / float(Globals.max_score_phase_1)
 	if result >= 56:
 		$PhasesGridContainer/Phase2Button.icon = load("res://assets/PhasesScreen/button_phase_2.png")
   
-  Audio.change_music(phase_1_musica)
+	Audio.change_music(phase_1_musica)
 
 # PT_BR: Abre a cena do level 1
 # EN_US: Opens the level 1 scene
