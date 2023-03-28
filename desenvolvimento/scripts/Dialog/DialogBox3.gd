5aff36f5640d996d827f0fe927c5b6176ec85d4d
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-03-28 15:18:59 -0300

    Adição dos comentários faltantes no DialogBox3.gd

--- desenvolvimento/scripts/Dialog/DialogBox3.gd
+++ desenvolvimento/scripts/Dialog/DialogBox3.gd
@@ -6,6 +6,8 @@ onready var text_element := $Text
 onready var timer := $Timer
 var counter = 0
 
+# PT_BR: Inicializa os sinais usados para passar de cena do diálogo
+# EN_US: Initializes signals used to step through dialog scenes
 signal finished_dialog()
 signal exit_dialog()
 
@@ -111,5 +113,7 @@ func _on_Timer_timeout():
 	text_element.visible_characters += 2
 
 
+# PT_BR: Função que volta para o diálogo anterior 
+# EN_US: Function that returns to the previous dialog
 func _on_BackButton_pressed():
 	show_message(-1)
