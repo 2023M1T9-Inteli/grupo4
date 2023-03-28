cd01441979f964cdc93db97492ed71db4cd82fb1
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-03-28 15:28:27 -0300

    Adição de boas práticas de espaços das funções no DialogScene3.gd

--- desenvolvimento/scripts/Dialog/DialogScene3.gd
+++ desenvolvimento/scripts/Dialog/DialogScene3.gd
@@ -5,6 +5,7 @@ extends Node2D
 func change_anim(anim):
 	$XarmesAnimatedSprite.animation = anim
 
+
 # PT_BR: Função que define se o sprite "Computer" deve aparecer ou ficar escondido 
 # EN_US: Function that defines if the sprite "Computer" should appear or stay hidden
 func show_tent(show_tent):
@@ -17,15 +18,18 @@ func show_tent(show_tent):
 	else:
 		$Gift.hide() 
 		
+		
 # PT_BR: Função que define que após o pressionar do botão, a cena é mudada para o 
 # EN_US: Function that defines that after the press of the button, the scene is changed
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/Menu.tscn")
 
+
 # PT_BR: Função que define que quando a cena de diálogo terminar, o jogo inicia.
 # EN_US: Function that defines that when the dialog scene ends, the game start.
 func _on_DialogBox_finished_dialog():
 	get_tree().change_scene("res://scenes/phase2/Phase2.tscn")
+
 
 func _on_DialogBox_exit_dialog():
 	get_tree().change_scene("res://scenes/Phases.tscn")
