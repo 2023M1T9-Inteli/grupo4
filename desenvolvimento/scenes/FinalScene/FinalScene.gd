3873ee1a62fa4ca956ac226afd8ab9089a5590e3
Pedro Morita Bannwart <pedro.bannwart@sou.inteli.edu.br>
2023-03-30 08:21:34 -0300

    Alteracoes restantes

---
+++ desenvolvimento/scenes/FinalScene/FinalScene.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	$BackgroundFinal/RichTextLabel.bbcode_text ="Você se provou um ótimo lider e uma pessoa com grandes competências em delegação de tarefas! Continue com o bom trabalho e lembre-se: Se um dia quiser praticar... Basta jogar[color=#d69856] O TALENTO DE DELEGAR![/color]"
