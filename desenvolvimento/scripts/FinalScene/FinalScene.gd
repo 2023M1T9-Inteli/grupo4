2d4c0e75c9631bbc9b9c962ec69411dacb84b3b3
Pedro Morita Bannwart <pedro.bannwart@sou.inteli.edu.br>
2023-03-30 18:24:59 -0300

    Jogo funcional com correcao de bugs

--- desenvolvimento/scripts/FinalScene/FinalScene.gd
+++ desenvolvimento/scripts/FinalScene/FinalScene.gd
@@ -5,4 +5,9 @@ onready var final_music = load("res://assets/Audio/music/VictoryMusic (mp3cut.ne
 func _ready():
 	Audio.play_music(final_music)
 	$CanvasLayer/AnimationPlayer.play("Fade")
-	$BackgroundFinal/RichTextLabel.bbcode_text ="Você se provou um ótimo lider e uma pessoa com grandes competências em delegação de tarefas! Continue com o bom trabalho e lembre-se: Se um dia quiser praticar... Basta jogar[color=#d69856] O TALENTO DE DELEGAR![/color]"
+	$BackgroundFinal/RichTextLabel.bbcode_text ="Você se provou um ótimo lider e uma pessoa com grandes competências em delegação de tarefas! Continue com o bom trabalho e lembre-se: Se um dia quiser praticar... Basta jogar:"
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn")
+
