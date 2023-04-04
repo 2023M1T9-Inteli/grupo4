cc47d666961013112c7e8c618b8fee8df545daf4
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-04-04 14:57:50 -0300

    Adição de espaçamentos e de comentários entre as funções no Pause.gd

--- desenvolvimento/scripts/Pause.gd
+++ desenvolvimento/scripts/Pause.gd
@@ -16,56 +16,50 @@ onready var options = $Options
 # EN_US: Stores the object in the variable
 onready var tutorial = $Tutorial
 
-# PT_BR: Variável booleana que armazena se o jogo está pausado.
-# EN_US: Boolean variable that stores whether the game is paused.
+# PT_BR: Variável booleana que armazena se o jogo está pausado
+# EN_US: Boolean variable that stores whether the game is paused
 var is_paused = false setget set_is_paused
 
 
-# PT_BR (1): Função executada sempre que a variável is paused é definida. 
-# PT_BR (2): Parâmetro: value int.
-# EN_US (1): Function executed every time that the variable is paused is defined. 
-# EN_US (2): Parameter: value int.
+# PT_BR (1): Função executada sempre que a variável is paused é definida
+# PT_BR (2): Parâmetro: value int
+# EN_US (1): Function executed every time that the variable is paused is defined
+# EN_US (2): Parameter: value int
 func set_is_paused(value: int):
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
 
 
-# PT_BR: Função executada quando o botão continuar é apertado para anular o pause.
-# EN_US: Function executed when the continue button is pressed to null the pause.
+# PT_BR: Função executada quando o botão continuar é apertado para anular o pause
+# EN_US: Function executed when the continue button is pressed to null the pause
 func _on_ContinueButton_pressed():
 	self.is_paused = false
 	self.hide()
 	depaused_sound.play()
-	#Audio.resume()
 	
 
-# PT_BR: Função executada quando o botão opções é apertado para mostrar o objeto Options.
-# EN_US: Function executed when the options button is pressed to show the Options object.
+# PT_BR: Função executada quando o botão opções é apertado para mostrar o objeto Options
+# EN_US: Function executed when the options button is pressed to show the Options object
 func open_pause_scene():
-#	Audio.stop()
 	paused_sound.play()
 	self.is_paused = true
 	
 
-# PT_BR: Função executada quando o botão sair é apertado que volta ao menu do jogo.
-# EN_US: Function executed when the exit button is pressed which returns to the game menu.
+# PT_BR: Função executada quando o botão sair é apertado que volta ao menu do jogo
+# EN_US: Function executed when the exit button is pressed which returns to the game menu
 func _on_QuitButton_pressed():
 	var __ = get_tree().change_scene("res://scenes/Menu.tscn")
-#	Audio.resume()
 	self.is_paused = false
 
 
+# PT_BR: Quando o botão tutorial é apertado que abre o popup do tutorial
+# EN_US: Executed when the tutorial button is pressed to open the tutorial popup
 func _on_TutorialButton_pressed():
-	# PT_BR: Quando o botão tutorial é apertado que abre o popup do tutorial
-	# EN_US: Executed when the tutorial button is pressed to open the tutorial popup.
 	tutorial.open_tutorial()
 
 
+# PT_BR: Abre o tela de opções
+# EN_US: Open the options screen
 func _on_OptionsButton_pressed():
-	# PT_BR: Abre o tela de opções
-	# EN_US: Open the options screen
 	$Options.show()
-
-
-
